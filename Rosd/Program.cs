196a9e4d50ee53a973c8fef9e499c4fb13b0802b
Dmitrii Evdokimov <diev@mail.ru>
2022-01-09 15:53:02 +0300

    Add PostgreSQL

--- Rosd/Program.cs
+++ Rosd/Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    // options.UseSqlServer(connectionString)); // Microsoft SQL
+    options.UseNpgsql(connectionString)); // PostgreSQL
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
