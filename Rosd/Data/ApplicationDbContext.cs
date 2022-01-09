07eb53bafccb3f6b6be25fbe4f066cb5b544dd17
Dmitrii Evdokimov <diev@mail.ru>
2022-01-09 18:41:39 +0300

    Change Names to snake_case

--- Rosd/Data/ApplicationDbContext.cs
+++ Rosd/Data/ApplicationDbContext.cs
@@ -1,13 +1,38 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Rosd.Data
+using Rosd.Helpers;
+
+namespace Rosd.Data;
+
+public class ApplicationDbContext : IdentityDbContext
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSnakeCaseNamingConvention();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("public");
+
+        base.OnModelCreating(builder);
+
+        foreach (var entity in builder.Model.GetEntityTypes())
         {
+            var tableName = builder.Entity(entity.Name).Metadata.GetDefaultTableName();
+            if (tableName != null)
+            {
+                if (tableName.Contains('<'))
+                {
+                    tableName = tableName.Split('<')[0];
+                }
+
+                builder.Entity(entity.Name).ToTable(tableName.ToSnakeCase());
+            }
         }
     }
 }
---
+++ Rosd/Helpers/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Rosd.Helpers;
+
+public static class StringExtensions
+{
+    public static string ToSnakeCase(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return input;
+        }
+
+        return string.Concat(input.Select((x, i) =>
+            char.IsUpper(x) && i > 0
+                ? "_" + x.ToString()
+                : x.ToString()))
+            .ToLower();
+    }
+}
