18b0a857664ce11055e3feba79c366e96f530a1c
Dmitrii Evdokimov <diev@mail.ru>
2022-01-15 20:43:03 +0300

    Add Context

---
+++ Rosd.Wpf/Data/TrackContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Rosd.Wpf.Models;
+
+namespace Rosd.Wpf.Data;
+
+public class TrackContext : DbContext
+{
+    public DbSet<Track>? Tracks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=tracks.db");
+        optionsBuilder.UseLazyLoadingProxies();
+    }
+}
